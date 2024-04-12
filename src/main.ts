f81e251221d1cc44682df6d9a982990563137ce3
fanciful-marmot <daniel.crookston@gmail.com>
2024-04-12 17:22:00 -0400

    Fix resize

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,22 @@ import Renderer from './renderer';
 
 const canvas = document.getElementById('gfx') as HTMLCanvasElement;
 const { width, height } = canvas.getBoundingClientRect();
-canvas.width = width;
-canvas.height = height;
+const dpr = window.devicePixelRatio;
+canvas.width = width * dpr;
+canvas.height = height * dpr;
+
+// Handle canvas resize
+const observer = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+  for (const entry of entries) {
+    if (entry.target === canvas) {
+      const dpr = window.devicePixelRatio;
+      const { width, height } = entry.contentRect;
+      canvas.width = width * dpr;
+      canvas.height = height * dpr;
+    }
+  }
+});
+observer.observe(canvas, { box: 'device-pixel-content-box' });
+
 const renderer = new Renderer(canvas);
 renderer.start();
